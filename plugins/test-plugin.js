056a20fc6e747acd29712cdbae54e9d12cfb5956
qiqzhao <bme_ritter@foxmail.com>
2024-07-15 15:44:53 +0800

    feat: test plugin hook

--- plugins/test-plugin.js
+++ plugins/test-plugin.js
@@ -11,6 +11,66 @@ class TestPlugin {
 
   apply(compiler) {
     console.log("Test plugin apply");
+
+    // environment是同步钩子，需要tap注册
+    compiler.hooks.environment.tap("TestPlugin", () => {
+      console.log("Test plugin environment");
+    });
+
+    // emit 是异步串行钩子
+    compiler.hooks.emit.tap("TestPlugin", (compilation) => {
+      console.log("Test plugin emit");
+    });
+
+    compiler.hooks.emit.tapAsync("TestPlugin", (compilation, callback) => {
+      setTimeout(() => {
+        console.log("Test plugin emit async");
+        callback();
+      }, 2000);
+    });
+
+    compiler.hooks.emit.tapPromise("TestPlugin", (compilation) => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          console.log("Test plugin emit async");
+          resolve();
+        }, 1000);
+      });
+    });
+
+    // make 是异步并行钩子 AsyncParalleHook
+    compiler.hooks.make.tapAsync(
+      "TestPlugin",
+      (compilation, callback) => {
+        setTimeout(() => {
+          console.log("Test plugin make 111");
+          callback();
+        });
+      },
+      3000
+    );
+
+    compiler.hooks.make.tapAsync(
+      "TestPlugin",
+      (compilation, callback) => {
+        setTimeout(() => {
+          console.log("Test plugin make 222");
+          callback();
+        });
+      },
+      1000
+    );
+
+    compiler.hooks.make.tapAsync(
+      "TestPlugin",
+      (compilation, callback) => {
+        setTimeout(() => {
+          console.log("Test plugin make 333");
+          callback();
+        });
+      },
+      2000
+    );
   }
 }
 
